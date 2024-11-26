b3bf697619d809a4a0aa9d1065482071d1a2495b
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-11-26 11:00:59 +0200

    Add files via upload

---
+++ ange 215 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 1. Найти минимальную величину из двух целых переменных a, b, 
+        /// используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+
+        static int Input (string mesage)
+        {
+            Console.Write(mesage);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int Min(int x,int y)
+        {
+            return x < y ? x : y;
+        }
+
+        public static void Qutput(int result)
+        {
+            Console.WriteLine(result);
+        }
+        
+        static void Main(string[] args)
+        {
+            int x = Input("x =");
+            int y = Input("y =");
+            int result = Min(x, y);
+            Qutput(result);
+            Console.Read();
+        }
+    }
+}
